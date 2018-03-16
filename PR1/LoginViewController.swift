1164031478eda564abe4d3f828f2916dd449a193
José Ángel Concepción <jaconcep@Macintosh-de-II1.local>
2018-03-16 15:47:00 +0000

    Initial project structure.

---
+++ PR1/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  PR1
+//
+//  Copyright © 2018 UOC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var welcomeLabel: UILabel!
+
+    // BEGIN-UOC-2
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "SegueToAuthentication", sender: self)
+    }
+    // END-UOC-2
+    
+    // BEGIN-UOC-5
+
+    // END-UOC-5
+    
+    // BEGIN-UOC-6
+
+    // END-UOC-6
+    
+    @IBAction func unwindToLogin(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        print("Unwind to login")
+    }
+}
