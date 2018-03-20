a7c5736b2b99cbac61c49e7cd49f7c52983e0cf5
José Ángel Concepción <jaconcep@Macintosh-de-II1.local>
2018-03-20 19:30:14 +0000

    Finished exercise 6.

--- PR1/LoginViewController.swift
+++ PR1/LoginViewController.swift
@@ -31,8 +31,9 @@ class LoginViewController: UIViewController {
     
     // BEGIN-UOC-5
     
-    // User and password are deleted
+    // User and password are deleted from form
     override func viewWillAppear(_ animated: Bool) {
+        
         usernameField.text = "";
         passwordField.text = "";
     }
@@ -40,6 +41,33 @@ class LoginViewController: UIViewController {
     // END-UOC-5
     
     // BEGIN-UOC-6
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Get current hour
+        let hour = Calendar.current.component(.hour, from: Date());
+        print(hour);
+        
+        // Checks the hour and set the corresponding values
+        if hour >= 6 && hour <= 11 {
+            welcomeLabel.text = "Good morning";
+            self.view.backgroundColor = UIColor.orange.lighter();
+        }
+        else if hour >= 12 && hour <= 17 {
+            welcomeLabel.text = "Good afternoon";
+            self.view.backgroundColor = UIColor.yellow.lighter();
+        }
+        else if hour >= 18 && hour <= 21 {
+            welcomeLabel.text = "Good evening";
+            self.view.backgroundColor = UIColor.red.lighter();
+        }
+        else if (hour >= 22 && hour <= 24) || (hour >= 0 && hour <= 5) {
+            welcomeLabel.text = "Good night";
+            self.view.backgroundColor = UIColor.blue.lighter();
+        }
+    }
 
     // END-UOC-6
     
