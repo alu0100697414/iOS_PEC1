68a38e473dbc7c6ce06a2c3135d64f9fb86650aa
José Ángel Concepción <jaconcep@Macintosh-de-II1.local>
2018-03-20 20:00:24 +0000

    Created oulets from exerxise 7.1.

--- PR1/CalculatorViewController.swift
+++ PR1/CalculatorViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     // BEGIN-UOC-7.1
+    
+    @IBOutlet weak var initialAmountLabel: UILabel!
+    @IBOutlet weak var interestRateLabel: UILabel!
+    @IBOutlet weak var yearsLabel: UILabel!
+    @IBOutlet weak var finalAmountSlider: UILabel!
+    @IBOutlet weak var initialAmountSlider: UISlider!
+    @IBOutlet weak var interestRateStepper: UIStepper!
+    @IBOutlet weak var yearsStepper: UIStepper!
 
     // END-UOC-7.1
     
