c91b24a1b1fc2f040da0acfc7000db6d672164fd
José Ángel Concepción <jaconcep@Macintosh-de-II1.local>
2018-03-20 18:16:36 +0000

    Finished exercise 4.

--- PR1/AuthenticationViewController.swift
+++ PR1/AuthenticationViewController.swift
@@ -8,8 +8,43 @@
 import UIKit
 
 class AuthenticationViewController: UIViewController, UITextFieldDelegate {
+    
     // BEGIN-UOC-4
-
+    
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var thirdField: UITextField!
+    @IBOutlet weak var fourthField: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        firstField.delegate = self;
+        secondField.delegate = self;
+        thirdField.delegate = self;
+        fourthField.delegate = self;
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.count + string.count - range.length
+        return newLength <= 1
+    }
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+        
+        let password = firstField.text! + secondField.text! + thirdField.text! + fourthField.text!;
+        
+        let loginAllowed = Services.validate( code: password );
+        
+        if loginAllowed == true {
+            performSegue(withIdentifier: "SegueToMainNavigation", sender: self);
+        } else {
+            Utils.show(Message: "Sorry, the entered code is not valid.", WithTitle: "Notification", InViewController: self)
+        }
+    }
+    
     // END-UOC-4
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
